b540a3b300ca1728715d782f3b90451bc7511b6c
Sam Jeibmann <sam.jeibmann@gmail.com>
2019-06-19 13:38:38 -0700

    updating time range

--- clock-countdown.js
+++ clock-countdown.js
@@ -1,6 +1,6 @@
 var day =["SUN", "MON", "TUES", "WEDS", "THURS", "FRI", "SAT"];
 
-var fD = new Date("April 16, 2019 00:00:00 GMT-0800");
+var fD = new Date("June 24, 2019 00:00:00 GMT-0800");
 var oneDay = 1000*60*60*24;
 
 var timeData = setInterval(function(){
